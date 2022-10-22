a3684998938a6f20ec65cc0f12a66f68c64d950c
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-10-22 18:55:11 +0600

    61-5 display news details

--- src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
+++ src/Pages/Shared/NewsSummaryCard/NewsSummaryCard.js
@@ -4,33 +4,37 @@ import { Link } from 'react-router-dom';
 import Image from 'react-bootstrap/Image';
 import { FaRegBookmark } from "react-icons/fa";
 import { FaShareAlt } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
+
 
 const NewsSummaryCard = ({ news }) => {
-    const { _id, title, author, total_view, details, image_url } = news;
+    const { _id, title, author, total_view, details, image_url, rating } = news;
     console.log(news);
     return (
         <Card className="text-center mb-5">
             <Card.Header className='d-flex justify-content-between'>
-                <div>
+                <div className='d-flex'>
                     <Image
                         roundedCircle
                         className='me-2'
-                        src={author.img}
+                        src={author?.img}
                         style={{ height: '60px' }}
                     ></Image>
                     <div>
-                        <p>{author.name}</p>
-                        <p>{author.published_date}</p>
+                        <p className='mb-0'>{author?.name}</p>
+                        <p>{author?.published_date}</p>
                     </div>
                 </div>
                 <div>
-                    <FaRegBookmark></FaRegBookmark>
+                    <FaRegBookmark className='me-2'></FaRegBookmark>
                     <FaShareAlt></FaShareAlt>
                 </div>
 
             </Card.Header>
             <Card.Body>
                 <Card.Title>{title}</Card.Title>
+                <Card.Img variant="top" src={image_url} />
                 <Card.Text>
                     {
                         details.length > 250 ?
@@ -40,7 +44,19 @@ const NewsSummaryCard = ({ news }) => {
                     }
                 </Card.Text>
             </Card.Body>
-            <Card.Footer className="text-muted">Total View: {total_view}</Card.Footer>
+            <Card.Footer className="text-muted d-flex justify-content-between">
+                <div>
+                    <FaStar className='text-warning me-2'></FaStar>
+                    <span>{rating?.number}</span>
+                </div>
+                <div>
+                    <FaEye className='me-2'></FaEye>
+                    <span>{total_view}</span>
+                </div>
+
+
+
+            </Card.Footer>
         </Card>
     );
 };
