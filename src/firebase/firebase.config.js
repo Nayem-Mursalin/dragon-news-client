1806f4ad5fa4ce85d5682bfa7fdf9b6ad9170016
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-10-23 16:54:22 +0600

    62-7 Added Environment Variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAmyzKb0Y09GmVIxL0M1rcR8xtIwF6DR18",
-    authDomain: "dragon-news-71b71.firebaseapp.com",
-    projectId: "dragon-news-71b71",
-    storageBucket: "dragon-news-71b71.appspot.com",
-    messagingSenderId: "671031888085",
-    appId: "1:671031888085:web:4deff1fb85df4de81d3a5c"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_,
 };
 
 // Initialize Firebase
