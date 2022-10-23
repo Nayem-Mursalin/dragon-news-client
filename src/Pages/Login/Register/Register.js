51939c657cec0685dbb15da6c6ef390958b8a817
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-10-23 14:52:21 +0600

    62-4 verify Email & Toast Added

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,13 +1,14 @@
 import React, { useContext, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import toast from 'react-hot-toast';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const Register = () => {
     const [error, setError] = useState('');
     const [accepted, setAccepted] = useState(false);
-    const { createUser, updateUserProfile } = useContext(AuthContext);
+    const { createUser, updateUserProfile, verifyEmail } = useContext(AuthContext);
 
     const handleSubmit = event => {
         event.preventDefault();
@@ -25,6 +26,8 @@ const Register = () => {
                 setError('');
                 form.reset();
                 handleUpdateUserProfile(name, photoURL);
+                handleEmailVerofication();
+                toast.success('Verify your Email Adress Before Log in');
             })
             .catch(e => {
                 console.error(e);
@@ -38,6 +41,15 @@ const Register = () => {
             photoURL: photoURL
         }
         updateUserProfile(profile)
+            .then(() => {
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+
+    const handleEmailVerofication = () => {
+        verifyEmail()
             .then(() => {
             })
             .catch(error => {
