8d8acab66969ca81fe9089ffcd38c154986b20c9
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-10-23 15:19:41 +0600

    62-5 <a> & <p> error handle

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -41,7 +41,7 @@ const Header = () => {
                         </NavDropdown>
                     </Nav>
                     <Nav>
-                        <Nav.Link href="#deets">
+                        <>
                             {
                                 user?.uid ?
                                     <>
@@ -55,7 +55,7 @@ const Header = () => {
                                     </>
                             }
 
-                        </Nav.Link>
+                        </>
                         <Nav.Link eventKey={2} href="#memes">
                             {
                                 user?.photoURL ?
